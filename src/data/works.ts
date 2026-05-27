7e7a76ae1a2223142e0fa2fc38cb55c8da33320b
Nicole Vicente <nicolevs94@gmail.com>
2026-05-27 21:55:10 +0200

    Add Works page with Tripulación, Estudio moments and Sessions.
    Introduce /works backed by data/works.ts and three new sections:
    TripulacionSection lists the crew, EstudioFotosSection shows the
    studio moments grid, and NoraiSessionsSection highlights the Norai
    Sessions block with the recurring quote. Reuse SiteNav, SpotifySection
    and the contact + visit footer to stay consistent with Servicios.

---
+++ src/data/works.ts
@@ -0,0 +1,64 @@
+export type CrewMember = {
+  name: string;
+  image: string;
+  bio: string;
+};
+
+export const crew: CrewMember[] = [
+  {
+    name: 'Aleix',
+    image: '/images/works/aleix.jpg',
+    bio: 'Con unas letras en un mallorquín del todo poético y un estilo de rock alternativo propio de Sheffield, nuestro “Aleix” Turner de la isla, se pasó por el estudio a dejar 4 delicias, que de momento se ha reservado para él. Esperamos que nos regale algo de su música pronto.',
+  },
+  {
+    name: 'Borja Palacios',
+    image: '/images/works/borja.jpg',
+    bio: 'El cantautor “pirata”, residente en Mallorca y con una gran trayectoria en los micros abiertos tanto en la isla como en Barcelona, vino a grabar la versión en directo, en vídeo de su canción “A veces”. Grabado junto a Marina Rueda, siempre será la 1a Norai Session, estrenada en nuestro canal de Youtube.',
+  },
+  {
+    name: 'CADMUS',
+    image: '/images/works/cadmus.jpg',
+    bio: 'Este proyecto de indie-rock, nace de dos amigos de Mallorca, con influencias parecidas y un gran gusto musical. El EP “The Rockets”, el cual mezclamos desde el estudio, les llevó a participar en el concurso Pop-Rock Palma, donde se llevaron el premio Mobo Fest, que les permitió actuar en el prestigioso festival.',
+  },
+  {
+    name: 'Can Friends',
+    image: '/images/works/can-friends.jpg',
+    bio: 'Banda residente en Madrid, que fusiona estilos folk, ritmos latinoamericanos y música de cantautor, creando una experiencia musical sensorial, social y variada. Han viajado varias veces desde la península para venir a grabar al estudio, dando como resultado 3 fantásticos singles.',
+  },
+  {
+    name: 'DeMàr',
+    image: '/images/works/demar.jpg',
+    bio: 'Cantautor trotamundos, de origen italiano, residente en Mallorca y enamorado de la música española, DeMàr, vino a grabar su 1er EP en una versión en directo en Estudi Norai. Sus canciones hablan de sus historias y expresan de manera poética e intensa, como él las ha vivido y sentido.',
+  },
+  {
+    name: 'Fernando Gallostra',
+    image: '/images/works/fernando.jpg',
+    bio: 'Guitarrista clásico mallorquín, con una larga trayectoria con el instrumento. A través de las 6 cuerdas, nos transmite todo tipo de emociones, influidas por la isla y las personas que le rodean. Vino al estudio a grabar su preciosa canción “Empty Rooms” y ya tiene programada su vuelta al Estudi Norai.',
+  },
+  {
+    name: 'J.Suau',
+    image: '/images/works/jsuau.jpg',
+    bio: 'Uno de los proyectos en el que nos encomendaron la composición de una canción personalizada. Fue un regalo que Jaume le quiso hacer a su hermana, después de lograr quedarse embarazada. Jaume nos proporcionó detalles sobre ella y su familia y con ello, creamos una letra y una canción desde cero, que él reprodujo como sorpresa a su familia.',
+  },
+  {
+    name: 'Mercé Pons',
+    image: '/images/works/merce-pons.jpg',
+    bio: 'La compositora Mercé nos pidió colaboración en la composición y en la posterior interpretación, de la Banda Sonora Original de la película “Cien Libros Juntos”, dirigida por Margà Melià. Esta BSO nos ha dado muchas alegrías, como por ejemplo la nominación al Goya a mejor música y mejor canción original.',
+  },
+];
+
+export const studioPhotos = [
+  '/images/works/studio-1.jpg',
+  '/images/works/studio-2.jpg',
+  '/images/works/studio-3.jpg',
+  '/images/works/studio-4.jpg',
+  '/images/works/studio-5.jpg',
+  '/images/works/studio-6.jpg',
+];
+
+export const sessionsThumb = '/images/works/norai-session.jpg';
+
+export const sessionsQuote =
+  '‘’texto breve que describe y evalúa una obra (libro, película, música, etc.) o evento, combinando información expositiva con una opinión crítica y argumentada, con el fin de darla a conocer y orientar al público, ya sea para recomendarla o advertir sobre ella.’’';
+
+export const sessionsQuoteAuthor = 'Borja Palacios';
